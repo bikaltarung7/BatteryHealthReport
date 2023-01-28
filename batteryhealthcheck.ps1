eba317996fa1052206b5b3a9be332309ca892d78
Bikal Thapa <bikaltarung7@gmail.com>
2023-01-28 11:14:03 -0500

    Initial script to generate battery report

---
+++ batteryhealthcheck.ps1
@@ -0,0 +1,9 @@
+﻿$path = "D:\Documents\Battery"
+$filename = Get-Date -Format 'dd-MMM-yyyy'
+
+If(!(Test-Path -Path "D:\Documents\Battery"))
+{
+    New-Item  -ItemType Directory -Path $path
+}
+
+powercfg /batteryreport -output "D:\Documents\Battery\$filename.html"
